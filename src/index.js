8976cb57ddcb9bf9f0c843e7f3cb587d3486d9f4
Nimi Wariboko Jr <nimi@channelmeter.com>
2017-01-25 20:21:40 -0800

    Add tt to PropType

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { PropTypes } from 'react';
 const PropType = PropTypes.shape({
     t: PropTypes.func.isRequired,
     tc: PropTypes.func.isRequired,
+    tt: PropTypes.func.isRequired,
     tu: PropTypes.func.isRequired,
     tm: PropTypes.func.isRequired,
 });
